ffa3769e781b7585e92888f55ba3116526d513ef
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-02-16 19:30:40 +0800

    chore (route): remove delete teacher endpoint.

--- backend/app/routes/route.py
+++ backend/app/routes/route.py
@@ -204,22 +204,6 @@ def create_teacher(teacher: CreateTeacherRequest, db: db_dependency, current_use
     return {new_teacher, new_user}
 
 
-@router.delete('/teacher/{id}')
-def remove_teacher(id: str, db: db_dependency, current_user: user_dependency):
-    if current_user.role != "admin":
-        raise credential_exception
-    
-    teacher = db.query(models.Teacher).filter(models.Teacher.id == id).first()
-    if not teacher:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='Teacher account does not exists.'
-        )
-
-    db.delete(teacher)
-    db.commit()
-
-
 @router.delete('/user/{id}')
 def remove_user(id: str, db: db_dependency, current_user: user_dependency):
     if current_user.role != "admin":
