5fd6bbd89bab2cdf3381d31d06ddbecef8402cc6
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-02-20 12:49:24 +0800

    chore (page): rewrite not found page.

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function NotFoundPage() {
     return (
         <div>
-            <h1>404 Page not found</h1>
+            <h1>404 Not Found</h1>
             <p>{`Oh no! Something went wrong. =(`}</p>
             <Link to={'/login'}>Back to home</Link>
         </div>
