267b39b3bf0c0caea59ae83520640a8738a01287
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-02-27 12:06:42 +0800

    fix (route): route control of user token expires or invalid token

--- frontend/src/auth/ProtectedRoute.jsx
+++ frontend/src/auth/ProtectedRoute.jsx
@@ -3,10 +3,10 @@ import { Outlet, Navigate } from "react-router-dom"
 import { useAuth } from "../hooks/authQuery"
 
 export default function ProtectedRoute() {
-    const { data: user, isLoading, isError } = useAuth()
-    
+    const { data: user, isLoading, isError} = useAuth()
+
     if (isLoading) return <h1>Loading...</h1>
-    if (isError) return <Navigate to={"/login"} replace />
+    if (!user) return <Navigate to={"/login"} replace />
 
     return <Outlet />
 }
--- frontend/src/auth/RoleGuard.jsx
+++ frontend/src/auth/RoleGuard.jsx
@@ -6,7 +6,7 @@ export default function RoleGuard({ rolesAllowed = [] }) {
     const {data: user, isLoading, isError} = useAuth();
 
     if (isLoading) return <h1>Loading...</h1>
-    if (!user) return <Navigate to={"/login"} replace />
+    if (!user) return <Navigate to={'/'} replace />;
     if (!rolesAllowed.includes(user.role)) return <Navigate to={'/no-permision'} replace />
 
     return <Outlet />
