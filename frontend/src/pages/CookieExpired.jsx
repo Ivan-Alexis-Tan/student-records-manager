05e572f697cdf34edd0b683f3222515f76784390
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-02-28 16:36:19 +0800

    refactor (page): cookie expired page view

--- frontend/src/pages/CookieExpired.jsx
+++ frontend/src/pages/CookieExpired.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom"
 export default function CookieExpired() {
     return (
         <div className="session-cookie-expired">
-            <h1>Session cookie expired.</h1>
+            <h1>Session expired.</h1>
             <p>Login again to access.</p>
 
             <Link className="session-cookie-expired__login-again" 
                 to={'/login'}
                 >
-                <h2>Login again</h2>
+                <h2>Login</h2>
             </Link>
         </div>
     )
