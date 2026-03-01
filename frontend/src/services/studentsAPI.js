30206f00e7687fd9f5591ed302da3fbc93eb3e21
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-03-01 15:59:48 +0800

    refactor (fetch): change into axios fetching

--- frontend/src/services/studentsAPI.js
+++ frontend/src/services/studentsAPI.js
@@ -1,5 +1,6 @@
 import { api } from "./axiosAPI"
 
+// Auth
 export async function submitLogin(loginDetails) {
     const refined = new URLSearchParams()
     refined.append("username", loginDetails.email)
@@ -16,82 +17,6 @@ export async function submitLogin(loginDetails) {
     return res.json()
 }
 
-export async function createStudent(student_info) {
-    return api.post('/students', {...student_info})
-}
-
-export async function removeStudents(student_id) {
-    const res = await fetch(`http://localhost:8000/students/${student_id}`, {
-        method: "DELETE",
-        credentials: "include",
-    });
-
-    if (!res.ok) throw new Error('Removing student failed.');
-    return res.json()
-}
-
-export async function findStudent(id) {
-    return api.get(`/students/${id}`).then(res => res.data)
-}
-
-export async function getStudentSelfDetails() {
-    return api.get('/me/students').then(res => res.data)
-}
-
-export async function getQuizes(studentId) {
-    const res = await fetch(`http://localhost:8000/students/${studentId}/quizzes`, {
-        method: "GET",
-        credentials: "include"
-    });
-    
-    if (!res.ok) return null
-    return await res.json()
-}
-
-export async function updateQuizScore(payload) {
-    const submitted = {
-        date: payload.date,
-        score: payload.score,
-        total_items: payload.total_items,
-        unit: payload.unit,
-        topic: payload.topic,
-    }
-    const res = await fetch(`http://localhost:8000/quizzes/${payload.id}`, {
-        method: "PATCH",
-        credentials: "include",
-        headers: {"Content-type": "application/json"},
-        body: JSON.stringify((payload.id, submitted))
-    })
-
-    if (!res.ok) throw new Error(res.text())
-    return await res.json()
-}
-
-export async function createQuizRecord(payload) {
-    console.log(`payload =`, payload)
-    const res = await fetch(`http://localhost:8000/quizzes`, {
-        method: "POST",
-        credentials: "include",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(payload),
-    });
-
-    if (!res.ok) throw new Error(await res.text())
-    return await res.json()
-}
-
-export async function deleteQuiz(quizId) {
-    const res = await fetch(`http://localhost:8000/quizzes/${quizId}`, {
-        method: "DELETE",
-        credentials: "include",
-        headers: {'Content-Type': "application/json"},
-        body: JSON.stringify(quizId)
-    })
-
-    if (!res.ok) throw new Error(res.text());
-    return await res.json()
-}
-
 export async function createStudentAccount(reqDetails) {
     const new_user = {
         username: reqDetails.username,
@@ -100,38 +25,59 @@ export async function createStudentAccount(reqDetails) {
         password: reqDetails.password,
         student_id: reqDetails.studentId
     }
-    
-    const res = await fetch(`http://localhost:8000/auth/user?email=${new_user.email}&role=${new_user.role}`, {
-        method: "POST",
-        credentials: "include",
-        headers: {'Content-Type': "application/json"},
-        body: JSON.stringify(new_user)
-    })
-
-    if (!res.ok) throw new Error(await res.text());
-    return res.json()
+    return api.post(`/auth/user`, new_user)
 }
 
+// Me
+export async function getStudentSelfDetails() {
+    return api.get('/me/students').then(res => res.data)
+}
+
+// Students
+export async function createStudent(student_info) {
+    return api.post('/students', {...student_info})
+}
+
+export async function removeStudents(student_id) {
+    return api.delete(`/students/${student_id}`)
+}
+
+export async function findStudent(id) {
+    return api.get(`/students/${id}`).then(res => res.data)
+}
+
+export async function getQuizes(studentId) {
+    return api.get(`/students/${studentId}/quizzes`).then(res => res.data)
+}
+
+// Quizzes
+export async function updateQuizScore(payload) {
+    const submitted = {
+        date: payload.date,
+        score: payload.score,
+        total_items: payload.total_items,
+        unit: payload.unit,
+        topic: payload.topic,
+    }
+    return api.patch(`/quizzes/${payload.id}`, {...payload.id, ...submitted})
+}
+
+export async function createQuizRecord(payload) {
+    return api.post(`/quizzes`, payload)
+}
+
+export async function deleteQuiz(quizId) {
+    return api.delete(`quizzes/${quizId}`)
+}
+
+// Teachers
 export async function createTeacherAccount(reqDetails) {
-    const res = await fetch(`http://localhost:8000/teachers`, {
-        method: "POST",
-        credentials: "include",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(reqDetails)
-    })
-
-    if (!res.ok) throw new Error(await res.text());
-    return res.json()
+    return api.post(`/teachers`, reqDetails)
 }
 
+// Users
 export async function deleteUserAccount(id) {
-    const res = await fetch(`http://localhost:8000/user/${id}`, {
-        method: "DELETE",
-        credentials: 'include',
-    })
-
-    if (!res.ok) throw new Error(await res.text());
-    return res.json()
+    return api.delete(`/user/${id}`)
 }
 
 export function capitalEveryWord(str, sep = ' ') {
