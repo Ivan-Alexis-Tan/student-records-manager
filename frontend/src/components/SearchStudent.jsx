e9f7ffb991021f5ab9761128d68b69b05720a225
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-03-05 15:08:20 +0800

    feat (student): create student searching react component

---
+++ frontend/src/components/SearchStudent.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react"
+import { capitalEveryWord } from "../services/helperFunctions"
+
+const attribs = ["last_name", "first_name", "id"]
+
+export default function SearchStudent({ 
+    setStateFn = () => {}, 
+    data = [],
+    searchLabel = 'Search: ',
+}) {
+    const [attribIdx, setAttribIdx] = useState(0)
+    const [attribKey, setAttribKey] = useState(attribs[attribIdx])
+    const [searchVal, setSearchVal] = useState('')
+    const [searchResults, setSearchResults] = useState([])
+
+    // Search Attrib Toggle
+    useEffect( () => {
+        setAttribKey(attribs[attribIdx % attribs.length])
+    }, [attribIdx])
+
+    // Search Auto-filtering
+    useEffect( () => {
+        if (searchVal !== '') {
+            setSearchResults(
+                data.reduce((acc, student) => {
+                    const loweredVal = searchVal.toLowerCase()
+                    if (student[attribKey].toLowerCase().startsWith(loweredVal)) acc.push(student);
+                    return acc
+                }, [])
+            )
+        }
+        else setSearchResults([]);
+    }, [searchVal, data, attribKey])
+
+    return (
+        <div>
+            {/* Search Student Form */}
+            <div>
+                <label onClick={_ => {
+                    setAttribIdx(prev => prev + 1)
+                    setSearchVal('')
+                }}>{searchLabel}</label>
+                <input type="text"
+                    title="Search student"
+                    placeholder={capitalEveryWord(attribKey, '_')}
+                    value={searchVal}
+                    onChange={e => setSearchVal(e.target.value)}
+                />
+            </div>
+            
+            {/* Search Results */}
+            <div>
+                {(searchResults.length >= 1) && <ul>{searchResults.map(student => <li 
+                        key={student.id}
+                        onClick={_ => {
+                            setStateFn(student)
+                            setSearchVal(`${student.first_name} ${student.last_name}`)
+                        }}
+                        > {student.first_name} {student.last_name}
+                    </li>
+                )}</ul>}
+            </div>
+        </div>
+    )
+}
