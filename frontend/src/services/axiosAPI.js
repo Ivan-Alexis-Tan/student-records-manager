c1f815bccec6517a4cba026e585eb6f98125d498
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-03-10 17:01:58 +0800

    feat (fetch): change logout request and add response configs.

--- frontend/src/services/axiosAPI.js
+++ frontend/src/services/axiosAPI.js
@@ -14,20 +14,20 @@ api.interceptors.response.use(
     (error) => {
         const status = error.response?.status
         const errorDetail = error.response?.data.detail ?? null
+        const configs = error.response.config
+        console.log(`request =`, configs.method.toUpperCase(), configs.url)
 
-        if (status === 401 && (errorDetail === 'INVALID_TOKEN' || errorDetail === "TOKEN_EXPIRED")) {
-            try {
-                api.post(`${baseUrl}/auth/logout`)
+        if (status === 401) {
+            if (isLoggedIn) {
+                isLoggedIn = false
+                await axios.post(`http://localhost:8000/auth/logout`)
+                console.log(`User logged out. ${configs.method.toUpperCase()} ${configs.url}`)
+                // queryClient.setQueryData(['auth', 'me'], null)
+                // queryClient.cancelQueries()
             }
-            catch(_) {}
         }
         
-        if (status == 401) {
-            queryClient.setQueryData(['auth', 'me'], null)
-
-            console.error(status, `(${error.response.statusText}) ${errorDetail}`)
-        }
-        
+        console.error(status, `${errorDetail} (${configs.method.toUpperCase()} ${configs.url})`)
         return Promise.reject(error)
     }
 )
