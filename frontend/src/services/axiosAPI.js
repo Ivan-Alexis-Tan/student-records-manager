9565e104b79b183c46b926d17eb40a535f17d096
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-03-10 17:39:18 +0800

    feat (fetch): add logout check

--- frontend/src/services/axiosAPI.js
+++ frontend/src/services/axiosAPI.js
@@ -9,17 +9,22 @@ export const api = axios.create({
 
 api.interceptors.response.use(
     (response) => {
-        const checked = {
+        const loginCheck = {
             status: response.status === 200,
             loggedIn: isLoggedIn === false,
             method: response.config.method === 'get',
             url: response.config.url === '/auth/me'
         }
 
-        if (Object.values(checked).every(req => req === true)) {
-            isLoggedIn = true;
-            console.log(`User logged in`)
+        const logoutCheck = {
+            status: response.status === 204,
+            loggedIn: isLoggedIn === true,
+            method: response.config.method === 'post',
+            url: response.config.url === '/auth/logout'
         }
+
+        if (Object.values(loginCheck).every(req => req === true)) isLoggedIn = true;
+        if (Object.values(logoutCheck).every(req => req === true)) isLoggedIn = false;
 
         return response
     },
