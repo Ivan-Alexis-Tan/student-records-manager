a23830bad1d4e18b8986d50388d9e7f38100d18d
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-03-18 17:38:42 +0800

    feat (admin): create grant and reject button UI.

--- frontend/src/components/RegisRequestTable.jsx
+++ frontend/src/components/RegisRequestTable.jsx
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
+import { useState } from "react"
+
 import { getRegisRequests } from "../services/studentsAPI"
 
 export default function RegistrationRequestTable() {
@@ -9,14 +11,38 @@ export default function RegistrationRequestTable() {
             return res.data
         },
         retry: false,
-    })
+    });
+
+    const [isGranting, setIsGranting] = useState(false)
+    const [isRejecting, setIsRejecting] = useState(false)
 
     if (isLoading) return <h1>Loading requests...</h1>
+
+    function handleGrant(requestObj) {
+        const grant = window.confirm(`Grant ${requestObj.username}'s requst?`)
+
+        if (!grant) return null
+        console.log(grant)
+    }
+
+    function handleReject(requestObj) {
+        const reject = window.confirm(`Reject ${requestObj.username}'s requst?`)
+    }
 
     return (
         <div>
             <h1>Registration Requests</h1>
 
+            {(!isRejecting) && <button onClick={_ => setIsGranting(prev => !prev)}
+                title={isGranting ? "Cancel granting requests" : "Grant request"}
+                >{isGranting ? "❌" : "Grant"}
+            </button>}
+
+            {(!isGranting) && <button onClick={_ => setIsRejecting(prev => !prev)}
+                title={isRejecting ? "Cancel rejecting requests" : "Reject request"}
+                >{isRejecting? "❌" : "Reject"}
+            </button>}
+            
             {(data.length >= 1) 
                 ? <table>
                     <thead>
@@ -24,7 +50,6 @@ export default function RegistrationRequestTable() {
                             <th>Role</th>
                             <th>Email</th>
                             <th>Username</th>
-                            <th>Student ID</th>
                             <th>First Name</th>
                             <th>Last Name</th>
                             <th>Field Specialty</th>
@@ -32,10 +57,18 @@ export default function RegistrationRequestTable() {
                     </thead>
                     <tbody>
                         {data.map(req => <tr key={req.id}>
-                            <td>{req.role}</td>
+                            <td>{isGranting && <button 
+                                    onClick={_ => handleGrant(req)} title="Grant request"
+                                    >✔️</button>
+                                } {isRejecting && <button 
+                                    onClick={_ => handleReject(req)} 
+                                    title="Reject request"
+                                    >🗑️</button>
+                                } {req.role}
+                            </td>
+
                             <td>{req.email}</td>
                             <td>{req.username}</td>
-                            <td>{req.student_id ?? "-"}</td>
                             <td>{req.first_name ?? "-"}</td>
                             <td>{req.last_name ?? "-"}</td>
                             <td>{req.field_specialty ?? "-"}</td>
