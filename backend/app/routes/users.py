7dc4b36224322e217e838114a7749253a0341ce5
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-03-19 13:43:36 +0800

    refactor (route): transfer `/user` routes to `user.py` files

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -4,16 +4,18 @@ from starlette import status
 import app.models.models as models
 
 from app.auth.dependencies import (
-    user_dependency, 
+    user_dependency,
+    db_dependency,
     credential_exception, 
     permission_exception,
 )
-from app.db import db_dependency
+from app.auth.auth import hash_password
+from app.schemas.auth import CreateUserRequest, CreateAdminRequest
 
-users_router = APIRouter()
+users_router = APIRouter(prefix='/users', tags=['users'])
 
 # API Endpoints
-@users_router.get("/users")
+@users_router.get("")
 def get_users(current_user: user_dependency, db: db_dependency):
     if not current_user:
         raise credential_exception
@@ -24,7 +26,62 @@ def get_users(current_user: user_dependency, db: db_dependency):
     return db.query(models.User).all()
 
 
-@users_router.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@users_router.post('', status_code=status.HTTP_201_CREATED)
+def create_user(payload: CreateUserRequest, current_user: user_dependency, db: db_dependency):
+
+    if not current_user:
+        raise credential_exception
+    
+    if db.query(models.User).filter(models.User.email == payload.email).first():
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User already exists."
+        )
+    
+    student = db.query(models.Student).filter(models.Student.id == payload.student_id).first()
+
+    if not student:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Student does not exists."
+        )
+    
+    student.user = models.User(
+        username = payload.username,
+        email = payload.email,
+        hashed_password = hash_password(payload.password),
+        role = payload.role
+    )
+
+    db.add(student.user)
+    db.commit()
+    db.refresh(student)
+
+    return {"message": "Successfully created new user account."}
+
+
+@users_router.post("/admin", status_code=status.HTTP_201_CREATED)
+def create_admin(payload: CreateAdminRequest, db: db_dependency):
+    if payload.password != payload.confirm_pw:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            detail="Password does not match."
+        )
+
+    user = models.User(
+        username = payload.username,
+        email = payload.email,
+        hashed_password = hash_password(payload.password),
+        role = payload.role
+    )
+
+    db.add(user)
+    db.commit()
+
+    return {"message": "Successfully created a new admin."}
+
+
+@users_router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def remove_user(id: str, db: db_dependency, current_user: user_dependency):
     if current_user.role != "admin":
         raise credential_exception
@@ -38,5 +95,3 @@ def remove_user(id: str, db: db_dependency, current_user: user_dependency):
 
     db.delete(user)
     db.commit()
-
-    
