2420ddb0ecde3b7f5931e3625fd5fea5afaa5b4f
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-03-19 13:16:54 +0800

    refactor (route): transfered quizzes routing files to separate file

--- backend/app/routes/route.py
+++ backend/app/routes/route.py
@@ -9,9 +9,6 @@ from app.auth.dependencies import (
     permission_exception,
 )
 from app.db import db_dependency
-from app.models.models import Quiz
-from app.basemodels import QuizModel, UpdateQuiz
-
 
 router = APIRouter()
 
@@ -25,82 +22,6 @@ def get_users(current_user: user_dependency, db: db_dependency):
         raise permission_exception
 
     return db.query(models.User).all()
-
-
-@router.patch('/quizzes/{id}', status_code=status.HTTP_200_OK)
-def update_quiz_score(id: str, payload: UpdateQuiz, db: db_dependency, current_user: user_dependency) -> None:
-    if not current_user:
-        raise credential_exception
-    
-    if current_user.role not in {"teacher", "admin"}:
-        raise credential_exception
-
-    allowed = {'date', 'score', 'total_items', 'unit', 'topic'}
-    details = payload.model_dump(exclude_unset=True)
-
-    if details['score'] > details['total_items']:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, 
-            detail="Score must not be greater than highest possible score."
-        )
-    
-    quiz = db.get(Quiz, id)
-    if not quiz:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Quiz not found."
-        )
-
-    for col, val in details.items():
-        if col in allowed:
-            setattr(quiz, col, val)
-
-    db.commit()
-    db.refresh(quiz)
-    return quiz
-
-
-@router.post('/quizzes', status_code=status.HTTP_201_CREATED)
-def add_quiz_record(payload: QuizModel, db: db_dependency, current_user: user_dependency):
-    if not current_user:
-        raise credential_exception
-    
-    if current_user.role not in {"teacher", "admin"}:
-        raise permission_exception
-    
-    new_quiz = Quiz(**payload.model_dump())
-    db.add(new_quiz)
-    db.commit()
-    db.refresh(new_quiz)
-
-    return new_quiz
-
-
-@router.delete('/quizzes/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_quiz(id: str, db: db_dependency, current_user: user_dependency) -> None:
-    """
-    - `id`
-        - Student quiz id
-        - `str` type
-    """
-    if not current_user:
-        raise credential_exception
-    
-    if current_user.role not in {"teacher", "admin"}:
-        raise permission_exception
-    
-    quiz = db.get(Quiz, id)
-
-    if not quiz:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail="Quiz not found."
-        )
-    
-    db.delete(quiz)
-    db.commit()
-
-    return {"message": "Successfully deleted a quiz record."}
 
 
 @router.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT)
