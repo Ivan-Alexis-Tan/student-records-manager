9c3f215bff2a5eacbc9464d3d7060877cd2b1077
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-03-20 23:23:12 +0800

    refactor (admin): simplify create account forms

--- frontend/src/pages/CreateStudentAccount.jsx
+++ frontend/src/pages/CreateStudentAccount.jsx
@@ -1,17 +1,21 @@
-import { Navigate, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
-import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
-import { mutationCreateStudentAcc } from "../hooks/mutateFuncs"
-import { api } from "../services/axiosAPI"
-import { useAuth } from "../hooks/authQuery"
 import SearchData from "../components/SearchData"
 
-const rolesAllowed = ['teacher', 'admin']
+import { mutationCreateStudentAcc } from "../hooks/mutateFuncs"
+import { schemaStudentForm } from "../schemas/schemas"
+import { api } from "../services/axiosAPI"
+import { capitalEveryWord } from "../services/helperFunctions"
 
 export default function CreateStudentAccountPage() {
-    const queryClient = useQueryClient()
-    const {data: user, isLoading} = useAuth()
+    const { register, handleSubmit, setValue, formState: { errors } } = useForm({
+        resolver: zodResolver(schemaStudentForm),
+        defaultValues: { role: 'student' },
+    })
     const { id } = useParams()
 
     const {data: students, isLoading: loadingStudents} = useQuery({
@@ -19,120 +23,86 @@ export default function CreateStudentAccountPage() {
         queryFn: () => api.get('/students').then(res => res.data),
     });
 
-    const {data: accounts, isLoading: loadingAccounts} = useQuery({
-        queryKey: ["users"],
-        queryFn: () => api.get('/users').then(res => res.data),
-        retry: false
-    })
-
     const createAccMutation = mutationCreateStudentAcc({
-        ifSuccess: () => {
-            setMessage({detail: `Successfully created ${selected.last_name}, ${selected.first_name}'s account.`, status: 'ok'})
-            setNewAccDetails(newAccDefault)
-        }
+        ifSuccess: () => setMessage({
+            detail: `Successfully created ${selected.last_name}, ${selected.first_name}'s account.`, 
+            ok: true
+        }),
+        ifError: (error) => setMessage({
+            ok: false,
+            detail: error.data.detail,
+        }),
     })
 
     const [selected, setSelected] = useState(students?.find(student => student.id === id))
+    const [message, setMessage] = useState({ok: false, detail: ""})
+    const [confirmPassword, setConfirmPassword] = useState('')
 
-    const newAccDefault = {
-        username: selected ? `${selected.first_name} ${selected.last_name}` : "",
-        email: "",
-        role: `student`,
-        password: "",
-        confirmPassword: "",
-        studentId: id ?? "",
-    }
-    const [newAccDetails, setNewAccDetails] = useState(newAccDefault)
-    const [message, setMessage] = useState({status: "", detail: ""})
+    const errorFields = Object.keys(errors);
+    const showConfirmPwError = (errorFields.length == 0) && message.detail
 
+    // Student ID setter
     useEffect(() => {
         if (!selected) return
-        setNewAccDetails(prev => ({
-            ...prev, 
-            studentId: selected.id,
-            username: `${selected.first_name} ${selected.last_name}`,
-        }))
+        
+        setValue('student_id', selected.id)
+        setValue('username', `${selected.first_name} ${selected.last_name}`)
     }, [selected])
 
-    if (isLoading) return <h1>Loading current user..</h1>
     if (loadingStudents) return <h1>Loading Students...</h1>
-    if (loadingAccounts) return <h1>Loading Accounts...</h1>
-    if (!accounts || !students) return <h1>Failed to load data, please retry.</h1>
+    if (!students) return <h1>Failed to load data, please retry.</h1>
 
-    function handleCreateAccount() {
-        const filled = Object.entries(newAccDetails).map(detail => detail[1] !== "")
-
-        if (!filled.every(f => f === true)) {
-            setMessage({detail: "ERROR: All fields must be filled.", status: 'error'})
-            console.error("ERROR: All fields must be filled.")
-            return null
+    function handleCreateAccount(data) {
+        if (data.password !== confirmPassword) {
+            setMessage({ok: false, detail: 'Confirm password does not match.'})
+            return
         }
-        if (newAccDetails.password !== newAccDetails.confirmPassword) {
-            setMessage({detail: 'ERROR: Password and confirm password does not match', status: 'error'})
-            console.error("ERROR: Password and confirm password does not match")
-            return null
-        }
-        
-        const emailExists = accounts.some(account => account.email === newAccDetails.email)
-        if (emailExists) {
-            setMessage({detail: 'ERROR: Account already exists.', status: 'error'})
-            console.error("ERROR: Account already exists.")
-            return null
-        }
-        if (!rolesAllowed.includes(user.role)) return <Navigate to={'/no-permision'} />
-        
-        createAccMutation.mutate(newAccDetails)
+        createAccMutation.mutate(data)
     }
-
+    
     return (
         <div>
             <h1>Create Student Account</h1>
-            {message.detail !== "" && <p style={message.status === "ok" ? statusOkStyle : statusErrorStyle}><strong>{message.detail}</strong></p>}
+            {showConfirmPwError && <p style={message.ok ? statusOkStyle : statusErrorStyle}><strong>{message.detail}</strong></p>}
+            {errorFields.length >= 1 && (
+                <div style={ statusErrorStyle }>
+                    <h4>Invalid {capitalEveryWord(errorFields[0], '_')}</h4>
+                    <p>{errors[errorFields[0]].message}</p>
+                </div>
+            )}
 
-            <form onKeyUp={e => {
-                if (e.key !== 'Enter') return null;
-                handleCreateAccount()
-            }}>
-                {!id && <SearchData setStateFn={setSelected} data={students ?? []} searchLabel="Create for: " />}
+            {!id && <SearchData setStateFn={setSelected} data={students ?? []} searchLabel="Create for: " />}
+            <br />
+
+            <form onSubmit={ handleSubmit(handleCreateAccount) } style={{ textAlign: 'center' }}>
+                <input type="text" placeholder="Username" {...register("username")} />
                 <br />
 
-                <input type="text"
-                    placeholder="username"
-                    value={newAccDetails.username}
-                    onChange={e => setNewAccDetails(prev => ({...prev, username: e.target.value}))}
-                />
+                <input type="email" placeholder="Email" {...register('email')} />
                 <br />
-                
-                <input type="text"
-                    placeholder="email"
-                    value={newAccDetails.email}
-                    onChange={e => setNewAccDetails(prev => ({...prev, email: e.target.value}))}
-                />
+
+                <input type="password" placeholder="Password" {...register("password")} />
                 <br />
 
                 <input type="password"
-                    placeholder="password"
-                    value={newAccDetails.password}
-                    onChange={e => setNewAccDetails(prev => ({...prev, password: e.target.value}))}
+                    placeholder="Confirm password"
+                    value={confirmPassword}
+                    onChange={e => setConfirmPassword(e.target.value)}
                 />
                 <br />
 
-                <input type="password"
-                    placeholder="confirm password"
-                    value={newAccDetails.confirmPassword}
-                    onChange={e => setNewAccDetails(prev => ({...prev, confirmPassword: e.target.value}))}
-                />
-                <br />
+                <button type="submit">Create Account</button>
             </form>
-            <button onClick={handleCreateAccount}>Create Account</button>
         </div>
     )
 }
 
 const statusOkStyle = {
-    color: "hsl(143, 100%, 60%)"
+    color: "hsl(143, 100%, 60%)",
+    textAlign: 'center',
 }
 
 const statusErrorStyle = {
-    color: "hsl(0, 100%, 60%)"
+    color: "hsl(0, 100%, 60%)",
+    textAlign: 'center',
 }
--- frontend/src/pages/CreateTeacherAccount.jsx
+++ frontend/src/pages/CreateTeacherAccount.jsx
@@ -1,150 +1,59 @@
 import { useEffect, useState } from "react"
 
-import { capitalEveryWord, subjects } from "../services/helperFunctions"
 import { mutationCreateTeacherAcc } from "../hooks/mutateFuncs"
+import RegisFormTeacher from "../components/RegisFormTeacher"
+
+const messageDefault = {
+    text: "",
+    ok: false,
+}
 
 export default function CreateTeacherAccountPage() {
-    const newAccDefault = {
-        first_name: "",
-        last_name: "",
-        field_specialty: subjects[0],
-        username: "",
-        email: "",
-        password: "",
-        role: "teacher"
-    }
-    const [newAccDetails, setNewAccDetails] = useState({...newAccDefault})
-    const [confirmPw, setConfirmPw] = useState("")
-    const [message, setMessage] = useState('')
-    const [messageStatus, setMessageStatus] = useState(null)
-    const [messageStyle, setMessageStyle] = useState({display: 'none'})
-
-    useEffect(() => {
-        if (messageStatus === "success") {
-            setMessageStyle({color: "hsl(135, 100%, 50%)"})
-        }
-        else if (messageStatus === "fail") {
-            setMessageStyle({color: "hsl(0, 100%, 69%)"})
-        } 
-        else {
-            setMessageStyle({display: 'none'})
-        }
-
-        return () => setMessageStyle({display: 'none'})
-
-    }, [messageStatus])
+    const [newAccDetails, setNewAccDetails] = useState()
+    const [message, setMessage] = useState(messageDefault)
+    const [formError, setFormError] = useState()
 
     const createTeacherAccMutation = mutationCreateTeacherAcc({
-        ifSuccess: () => {
-            setMessage(`Successfully created ${newAccDetails.username}'s account.`)
-            setMessageStatus('success')
-            setNewAccDetails({...newAccDefault})
-            setConfirmPw("")
-        }
+        ifSuccess: _ => {
+            setMessage({
+                text: `Successfully created ${newAccDetails.username}'s account.`, 
+                ok: true
+            });
+            setNewAccDetails()
+        },
+        ifError: (error) => setMessage({
+            ok: false,
+            text: error.data.detail
+        }),
     })
     
-    function handleSubmit() {
-        const emptyEntry = Object.entries({...newAccDetails, confirmPw: confirmPw}).reduce((acc, [key, value]) => {
-            if (!value) acc.push(key);
-            return acc
-        }, [])
-
-        if (emptyEntry.length >= 1) {
-            setMessage('ERROR: Every field must be filled.')
-            setMessageStatus('fail')
-            return null
-        }
-        if (newAccDetails.password !== confirmPw) {
-            setMessage('ERROR: Password confirmation does not match.')
-            setMessageStatus('fail')
-            console.error('failed desu')
-            return null
-        }
+    // Create Teacher Account Mutation
+    useEffect(() => {
+        if (!newAccDetails) return 
 
         createTeacherAccMutation.mutate(newAccDetails)
-    }
+    }, [newAccDetails])
+
+    // Error message updater
+    useEffect(() => {
+        if (formError) setMessage(messageDefault);
+    }, [formError])
 
     return (
         <div>
             <h1>Create Teacher Account</h1>
-            {message && <h3 style={messageStyle}>{message}</h3>}
-            <form>
-                <h3>Teacher Profile:</h3>
-                <input type="text"
-                    title="First Name"
-                    placeholder="First Name"
-                    value={newAccDetails.first_name}
-                    onChange={e => setNewAccDetails(prev => ({...prev, first_name: capitalEveryWord(e.target.value)}))}
-                />
-                <br />
-                
-                <input type="text"
-                    title="Last Name"
-                    placeholder="Last Name"
-                    value={newAccDetails.last_name}
-                    onChange={e => setNewAccDetails(prev => ({...prev, last_name: capitalEveryWord(e.target.value)}))}
-                />
-                <br />
 
-                <label>Area of Specialization: </label>
-                <br />
+            {message.text && <p style={message.ok
+                ? {color: 'hsl(113, 100%, 50%)', textAlign: 'center'}
+                : {color: 'hsl(9, 100%, 69%)', textAlign: 'center'}
+            }>{message.text}</p>}
 
-                <select name="field-specialization"
-                    title="Area of Specialization"
-                    value={newAccDetails.field_specialty} 
-                    onChange={e => setNewAccDetails(prev => ({...prev, field_specialty: e.target.value}))}
-                    >
-                    {subjects.map(subj => <option key={subj} value={subj}>{subj}</option>)}
-                </select>
-                <br />
-                
-                <h3>Teacher Account Details:</h3>
-                <input type="text"
-                    title="Username"
-                    placeholder="Username"
-                    value={newAccDetails.username}
-                    onChange={e => setNewAccDetails(prev => ({...prev, username: e.target.value}))}
-                />
-                <button type="button" 
-                    onClick={_ => {
-                        setNewAccDetails(prev => ({...prev, username: `${newAccDetails.first_name} ${newAccDetails.last_name}`}))
-                    }}
-                    title="Set fullname as username."
-                >Set Fullname</button>
-                <br />
-                
-                <input type="text"
-                    title="Email"
-                    placeholder="Email"
-                    value={newAccDetails.email}
-                    onChange={e => setNewAccDetails(prev => ({...prev, email: e.target.value}))}
-                />
-                <br />
-
-                <input type="password"
-                    title="Password"
-                    placeholder="Password"
-                    value={newAccDetails.password}
-                    onChange={e => setNewAccDetails(prev => ({...prev, password: e.target.value}))}
-                />
-                <br />
-
-                <input type="password"
-                    title="Confirm password"
-                    placeholder="Confirm password"
-                    value={confirmPw}
-                    onChange={e => setConfirmPw(e.target.value)}
-                />
-                <br />
-
-                <button 
-                    onClick={handleSubmit} 
-                    type="button"
-                    title={`Create account`}
-                >Create</button>
-            </form>
-
-            {/* <button onClick={handleSubmit}>test</button> */}
+            <RegisFormTeacher configs={{ 
+                setFn: setNewAccDetails, 
+                centerErrMsg: true, 
+                centerForm: true,
+                errorsStateFn: setFormError,
+            }} />
         </div>
     )
 }
