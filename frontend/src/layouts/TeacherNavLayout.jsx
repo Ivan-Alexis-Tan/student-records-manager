6c8f59d58910fb2d6119a25c5b113345aae958da
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-08-10 23:01:35 +0800

    refactor (nav): styles and responsive layout

--- frontend/src/layouts/TeacherNavLayout.jsx
+++ frontend/src/layouts/TeacherNavLayout.jsx
@@ -14,7 +14,7 @@ export default function TeacherNavLayout() {
 
             <div>
                 <Link to={`/teacher/${user.profile_id}`}>{user.username}</Link>
-                <Link to={'/add_student'}>Add Student</Link>
+                <Link to={'/add_student'}>➕ Student</Link>
                 <h3 className='nav__logout'
                     onClick={_ => {
                     const confirmLogout =  window.confirm('Confirm logout.')
